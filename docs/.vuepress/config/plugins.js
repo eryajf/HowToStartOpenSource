fa77ca685e72708a345a30938e2282f226ce4bb6
二丫讲梵 <eryajf@163.com>
2022-07-20 13:48:37 +0800

    添加vssue评论

--- docs/.vuepress/config/plugins.js
+++ docs/.vuepress/config/plugins.js
@@ -5,7 +5,7 @@ module.exports = [
       '@vssue/vuepress-plugin-vssue',
       {
         // 设置 `platform` 而不是 `api`
-        platform: 'github',
+        platform: 'github-v4',
 
         // 其他的 Vssue 配置
         owner: 'eryajf',
