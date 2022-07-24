2dac5fb8da47569115b546bef0837ace9989e6b0
二丫讲梵 <eryajf@163.com>
2022-07-24 16:12:19 +0800

    🎊 添加里程碑与开源项目汇总

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -4,6 +4,10 @@ module.exports = [
   { text: '基础内容',link: '/basic/',},
   { text: 'GitHub-Actions',link: '/github-actions/',},
   { text: 'GitHub-Tips', link: '/github-tips/' },
-  { text: '留言板', link: '/message-board/' },
+  { text: '留言板', link: '/message-board/',
+    items: [
+      { text: '我的开源项目汇总', link: '/myopensource/' },
+    ],
+  },
   { text: '我的博客', link: 'https://wiki.eryajf.net' },
 ]
