c1b71f2c9ec9502b4db3e339e01755d4a46c0f7a
eryajf <eryajf@163.com>
2023-02-16 22:53:27 +0800

    fix en nav bug

--- docs/.vuepress/config/nav/index.js
+++ docs/.vuepress/config/nav/index.js
@@ -8,10 +8,10 @@ module.exports = {
     { text: 'GitHub', link: 'https://github.com/eryajf/HowToStartOpenSource', icon: 'reco-github'}
   ],
   'en': [
-    { text: 'basic-content', link: '/views/01-basic-content/01-basic-configuration', icon: 'reco-blog'  },
-    { text: 'Github-Actions', link: '/views/02-github-actions/01-Automatically-add-TOC-directories-for-README' , icon: 'reco-up'},
-    { text: 'Github-Tips', link: '/views/03-github-tips/01-How-to-center-the-table-in-README',icon: 'reco-three' },
-    { text: 'myopensource', link: '/views/other/myopensource',icon: 'reco-github' },
+    { text: 'basic-content', link: '/en/views/01-basic-content/01-basic-configuration', icon: 'reco-blog'  },
+    { text: 'Github-Actions', link: '/en/views/02-github-actions/01-Automatically-add-TOC-directories-for-README' , icon: 'reco-up'},
+    { text: 'Github-Tips', link: '/en/views/03-github-tips/01-How-to-center-the-table-in-README',icon: 'reco-three' },
+    { text: 'myopensource', link: '/en/views/other/myopensource',icon: 'reco-github' },
     { text: 'RSS', link: 'https://eryajf.github.io/HowToStartOpenSource/rss.xml', icon: 'reco-rss'},
     { text: 'GitHub', link: 'https://github.com/eryajf/HowToStartOpenSource', icon: 'reco-github'}
   ]
