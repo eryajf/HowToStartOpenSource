ee1766dbefe9b04ce973cb870c680efffadcc3e2
eryajf <eryajf@163.com>
2023-02-18 15:01:20 +0800

    fix build error

---
+++ docs/.vuepress/data/pluginsData.js
@@ -0,0 +1,139 @@
+export default [
+  {
+    name: 'vuepress-plugin-sponsor',
+    desc: '打赏插件',
+    user: 'yokefellow',
+    repo: 'vuepress-plugin-sponsor',
+    feature: 'feature',
+    scenes: 'independent'
+  },
+  { 
+    name: '@vuepress-reco/back-to-top', 
+    desc: '回到顶部。',
+    user: 'vuepress-reco',
+    repo: 'vuepress-plugin-back-to-top',
+    feature: 'feature',
+    scenes: 'independent'
+  },
+  { 
+    name: '@vuepress-reco/pagation', 
+    desc: '分页。',
+    user: 'vuepress-reco',
+    repo: 'vuepress-plugin-pagation',
+    feature: 'feature',
+    scenes: 'develop'
+  },
+  { 
+    name: '@vuepress-reco/loading-page', 
+    desc: '加载 loading 页。',
+    user: 'vuepress-reco',
+    repo: 'vuepress-plugin-loading-page',
+    feature: 'style',
+    scenes: 'develop'
+  },
+  { 
+    name: '@vuepress-reco/kan-ban-niang', 
+    desc: '看板娘 live2d 动画。',
+    user: 'vuepress-reco',
+    repo: 'vuepress-plugin-kan-ban-niang',
+    feature: 'style',
+    scenes: 'develop'
+  },
+  { 
+    name: '@vuepress-reco/comments', 
+    desc: '评论功能，集成 valine 和 vssue。',
+    user: 'vuepress-reco',
+    repo: 'vuepress-plugin-comments',
+    feature: 'feature',
+    scenes: 'develop'
+  },
+  { 
+    name: '@vuepress-reco/extract-code', 
+    desc: '功能代码展示插件，展示多种语言于一窗，增加易读性。',
+    user: 'vuepress-reco',
+    repo: 'vuepress-plugin-extract-code',
+    feature: 'feature',
+    scenes: 'develop'
+  },
+  { 
+    name: '@vuepress-reco/bgm-player', 
+    desc: '一款简洁的音乐播放器。',
+    user: 'vuepress-reco',
+    repo: 'vuepress-plugin-bgm-player',
+    feature: 'feature',
+    scenes: 'independent'
+  },
+  { 
+    name: '@vuepress-reco/rss', 
+    desc: '生成 RSS 页面。',
+    user: 'vuepress-reco',
+    repo: 'vuepress-plugin-rss',
+    feature: 'feature',
+    scenes: 'independent'
+  },
+  {
+    //使用说明网址见：https://zpj80231.gitee.io/znote/views/front-end/vuepress-plugin-boxx.html
+    name: 'vuepress-plugin-boxx',
+    desc: '为博客文章自动随机添加名人名言或其他，可自定义样式和内容。',
+    user: 'zpj80231',
+    repo: 'vuepress-plugin-boxx',
+    feature: 'feature',
+    scenes: 'independent'
+  },
+  { 
+    name: 'go-top', 
+    desc: '回到顶部。',
+    user: 'SigureMo',
+    repo: 'vuepress-plugin-go-top',
+    feature: 'feature',
+    scenes: 'independent'
+  },
+  { 
+    name: 'ribbon', 
+    desc: '在您的 Vuepress 中添加美丽的丝带！',
+    user: 'SigureMo',
+    repo: 'vuepress-plugin-ribbon',
+    feature: 'style',
+    scenes: 'independent'
+  },
+  { 
+    name: 'cursor-effects', 
+    desc: '在您的 vuepress 中添加可爱的光标效果！',
+    user: 'SigureMo',
+    repo: 'vuepress-plugin-cursor-effects',
+    feature: 'style',
+    scenes: 'independent'
+  },
+  { 
+    name: 'dynamic-title', 
+    desc: '在您的 vuepress 中添加动态标题！',
+    user: 'SigureMo',
+    repo: 'vuepress-plugin-dynamic-title',
+    feature: 'style',
+    scenes: 'independent'
+  },
+  { 
+    name: 'dynamic-title', 
+    desc: '流程图。',
+    user: 'ulivz',
+    repo: 'vuepress-plugin-flowchart',
+    feature: 'feature',
+    scenes: 'independent'
+  },
+  { 
+    name: '@vuepress/plugin-medium-zoom', 
+    desc: '图片放大。',
+    user: 'vuejs',
+    repo: 'vuepress/tree/master/packages/@vuepress/plugin-medium-zoom',
+    feature: 'feature',
+    scenes: 'independent'
+  },
+  { 
+    name: 'sitemap', 
+    desc: 'Sitemap 生成器。',
+    user: 'ekoeryanto',
+    repo: 'vuepress-plugin-sitemap',
+    feature: 'feature',
+    scenes: 'independent'
+  }
+]
